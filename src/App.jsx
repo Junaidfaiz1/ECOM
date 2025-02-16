b2c5c426763d4a513db3e8737ddd2eae6d2a11de
Junaidfaiz1 <he630001@gmail.com>
2025-02-16 03:26:26 -0800

    packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import {react, lazy, Suspense} from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+const Home = lazy (()=>import("./Components/Home.jsx"))
+const Products = lazy (()=>import("./Components/Products.jsx"))
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <BrowserRouter>
+     <Suspense fallback = {<h2>Loading</h2>}>
+     <Routes>
+      <Route path="/Products" element={<Products />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </Suspense>
+  </BrowserRouter>
     </>
   )
 }
