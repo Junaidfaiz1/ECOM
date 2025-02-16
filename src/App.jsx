6adf708863649596fa5977b97b5d75ce08c8375f
Junaidfaiz1 <he630001@gmail.com>
2025-02-16 10:21:55 -0800

    new packages is installed and redux toolkit store and provider is writen

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import './App.css'
-import {react, lazy, Suspense} from "react"
+import "./App.css";
+import { react, lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-const Home = lazy (()=>import("./Components/Home.jsx"))
-const Products = lazy (()=>import("./Components/Products.jsx"))
+const Home = lazy(() => import("./Components/Home.jsx"));
+const Products = lazy(() => import("./Components/Products.jsx"));
+
+const Layout = () => {
+  return (
+    <>
+      <Home />
+      <Products />
+    </>
+  );
+};
+
 function App() {
   return (
     <>
-     <BrowserRouter>
-     <Suspense fallback = {<h2>Loading</h2>}>
-     <Routes>
-      <Route path="/Products" element={<Products />} />
-      <Route path="/" element={<Home />} />
-    </Routes>
-    </Suspense>
-  </BrowserRouter>
+      <BrowserRouter>
+        <Suspense fallback={<h2>Loading</h2>}>
+          <Routes>
+            <Route path="/" element={<Layout />} />
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Products" element={<Products />} />
+            <Route />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
